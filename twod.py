d119e6f930ecb2dfe4b79aecdda53b1a78885eb2
jessicagamio <jessica.gamio@gmail.com>
2019-12-06 17:26:24 -0800

    added hourglassSum function

--- twod.py
+++ twod.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 def makeArray(arr):
 
@@ -11,13 +13,31 @@ def makeArray(arr):
 
         if (i+1) % 6 == 0:
             array.append(subArray)
-            print('subarray ===>', subArray)
             subArray = []
           
 
     return array
 
 
+
+def hourglassSum(arr):
+    """returns the maximum sum of hourglass array"""
+
+    array=makeArray(arr)
+    # for num in array:
+    #     print(num)
+
+    maxNum=0
+
+    for i in range(0,4):
+        for j in range(0,4):
+            hourglassSum = sum(array[i][j:j+3]) + array[i+2][j+1] + sum(array[i+2][j:j+3])
+
+            if maxNum == 0:
+                maxNum = hourglassSum
+            elif hourglassSum > maxNum:
+                maxNum = hourglassSum
+    return maxNum
 
 
 
@@ -30,4 +50,28 @@ arr = "1 1 1 0 0 0 \
 0 0 0 2 0 0 \
 0 0 1 2 4 0"
 
+#answer is 28
+arr1= "-9 -9 -9 1 1 1 \
+0 -9 0 4 3 2 \
+-9 -9 -9 1 2 3 \
+0 0 8 6 6 0 \
+0 0 0 -2 0 0 \
+0 0 1 2 4 0"
 
+#answer is 13
+arr2="1 1 1 0 0 0 \
+0 1 0 0 0 0 \
+1 1 1 0 0 0 \
+0 9 2 -4 -4 0 \
+0 0 0 -2 0 0 \
+0 0 -1 -2 -4 0"
+
+
+class testFunction(unittest.TestCase):
+
+    def testMethod(self):
+        self.assertEqual(hourglassSum(arr),19)
+        self.assertEqual(hourglassSum(arr1),28)
+        self.assertEqual(hourglassSum(arr2),13)
+if __name__=="__main__":
+    unittest.main()
